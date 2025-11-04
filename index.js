fe795a87b4fa8af504a1b4e140434f6cc2d7cca3
AxiomWolf <geronimo.xl13@gmail.com>
2025-11-04 15:03:40 +0100

    Added Some Command Line Arguments

--- index.js
+++ index.js
@@ -1,64 +1,125 @@
-import xterm from '@xterm/xterm'
-const { Terminal } = xterm;
-import { SerializeAddon } from "@xterm/addon-serialize";
-import { spawn } from 'node-pty'
-import { WebSocketServer } from 'ws';
+import xterm from "@xterm/xterm"
+const { Terminal } = xterm
+import { SerializeAddon } from "@xterm/addon-serialize"
+import { spawn } from "node-pty"
+import { WebSocketServer } from "ws"
+
+const settings = {
+  silent: false,
+  command: "btop",
+  args: ["-u", "1000"],
+  port: 8080,
+}
+
+// Process command line arguments
+var argv = process.argv
+
+// Extract the overwrite command, if applicable
+if (argv.includes("-c")) {
+  const startIndex = argv.indexOf("-c")
+
+  // Get everything after the flag, removing it from the original array
+  const command = argv.splice(startIndex + 1)
+
+  if (command.length < 1) {
+    console.log("No overwrite command specified after command flag!")
+    process.exit(1)
+  }
+
+  argv.pop() // Remove the trailing flag
+
+  settings.command = command.shift()
+  settings.args = command
+}
+
+if (argv.includes("-h") || argv.includes("--help")) {
+  console.log(`
+This script will automatically send the output of btop to all connected websocket clients using Resonite's text markup.
+
+Usage: node index.js [OPTIONS] [-c COMMAND]
+
+-s\tSilent
+-p\tPort
+-c\tUse an alternate command (and/or arguments)
+-h, --help\tShows this help
+    `)
+  process.exit(0)
+}
+
+if (argv.includes("-s")) settings.silent = true
+
+if (argv.includes("-p")) {
+  const index = argv.indexOf("-p") + 1
+
+  if (argv.length - 1 < index) {
+    console.log("No port given!")
+    process.exit(1)
+  }
+
+  settings.port = parseInt(argv[index])
+}
 
 // Setup websocket
-const wss = new WebSocketServer({ port: 8080 });
-wss.on('connection', function connection(ws)
-{
-    ws.on('error', console.error);
+const wss = new WebSocketServer({ port: settings.port })
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error)
 
-    ws.on('message', function message(data)
-    {
-        console.log('received: %s', data);
-    });
-
-});
+  ws.on("message", function message(data) {
+    if(!settings.silent)
+    console.log("Received: %s", data)
+  })
+})
 
 // Create xterm.js instance
-const COLS = 120;
-const ROWS = 30;
-const term = new Terminal({ cols: COLS, rows: ROWS, scrollback: 0 }); 
-const serializeAddon = new SerializeAddon();
+const COLS = 120
+const ROWS = 30
+const term = new Terminal({ cols: COLS, rows: ROWS, scrollback: 0 })
+const serializeAddon = new SerializeAddon()
 term.loadAddon(serializeAddon)
-const btopProcess = spawn("btop", ["-u", "1000"], {
-    name: "xterm-color",
-    cols: COLS,
-    rows: ROWS,
-    cwd: process.cwd(),
-    env: process.env,
-    scrollback: 0
-});
+const btopProcess = spawn(settings.command, settings.args, {
+  name: "xterm-color",
+  cols: COLS,
+  rows: ROWS,
+  cwd: process.cwd(),
+  env: process.env,
+  scrollback: 0,
+})
 
-btopProcess.on("data", (data) =>
-{
-    term.write(data); // Pipe process output to xterm.js
+btopProcess.on("data", (data) => {
+  term.write(data) // Pipe process output to xterm.js
 
-    let serializedData = serializeAddon.serializeAsHTML({ scrollback: 0 })
+  let serializedData = serializeAddon.serializeAsHTML({ scrollback: 0 })
 
-    var result = serializedData.substring("<html><body><!--StartFragment--><pre><div style='color: #000000; background-color: #ffffff; font-family: courier-new, courier, monospace; font-size: 15px;'>".length, serializedData.length - "</pre><!--EndFragment--></body></html>".length)
+  var result = serializedData.substring(
+    "<html><body><!--StartFragment--><pre><div style='color: #000000; background-color: #ffffff; font-family: courier-new, courier, monospace; font-size: 15px;'>"
+      .length,
+    serializedData.length - "</pre><!--EndFragment--></body></html>".length,
+  )
 
-    // Trim unnecessary spans
-    result = result.replaceAll("<span></span>", "")
-    result = result.replaceAll("<div>", "")
-    result = result.replaceAll("</div>", "\n")
+  // Trim unnecessary spans
+  result = result.replaceAll("<span></span>", "")
+  result = result.replaceAll("<div>", "")
+  result = result.replaceAll("</div>", "\n")
 
-    // Convert color codes
-    result = result.replaceAll(/<span style='color: (#[0-9a-fA-F]{6}); background-color: (#[0-9a-fA-F]{6});(?: font-weight: (\w+);)?'>/g, (match, color, background, weight) => `<color ${color}><mark ${background}>${weight ? "<b>" : ""}`)
-    result = result.replaceAll(/<span style='background-color: (#[0-9a-fA-F]{6});'>/g, (match, background) => `<mark ${background}>`)
-    result = result.replaceAll("</span>", "<i></closeall>")
-    result = result.replaceAll("<span>", "")
+  // Convert color codes
+  result = result.replaceAll(
+    /<span style='color: (#[0-9a-fA-F]{6}); background-color: (#[0-9a-fA-F]{6});(?: font-weight: (\w+);)?'>/g,
+    (match, color, background, weight) =>
+      `<color ${color}><mark ${background}>${weight ? "<b>" : ""}`,
+  )
+  result = result.replaceAll(
+    /<span style='background-color: (#[0-9a-fA-F]{6});'>/g,
+    (match, background) => `<mark ${background}>`,
+  )
+  result = result.replaceAll("</span>", "<i></closeall>")
+  result = result.replaceAll("<span>", "")
 
-    // Send the string to each client
-    wss.clients.forEach(client =>
-    {
-        if (client.readyState == WebSocket.OPEN)
-        {
-            client.send(result)
-        }
-    })
+  // Send the string to each client
+  wss.clients.forEach((client) => {
+    if (client.readyState == WebSocket.OPEN) {
+      client.send(result)
+    }
+  })
 
-    console.log("Data sent.")
-});
+  if (!settings.silent) console.log("Data sent.")
+})
